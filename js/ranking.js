7dd7b78e19e8677954409f875f4a7374f776ac36
Amr Elnahas <amr.elnahas2005@gmail.com>
2023-09-27 23:41:16 +0300

    ECF BETA 27-09-2023

---
+++ js/ranking.js
@@ -0,0 +1,134 @@
+// Define Varaiables for LIs
+let accIcon = document.getElementById("account_icon");
+let aboutIcon = document.getElementById("about-icon");
+let workIcon = document.getElementById("work-icon");
+let mailIcon = document.getElementById("mail-icon");
+let overIcon = document.getElementById('over-icon');
+// Define Variables for Sections
+let manageAccount = document.getElementById('manageAccount');
+let notification = document.getElementById('notification');
+let favList = document.getElementById('favList');
+let ordersTracking = document.getElementById('ordersTracking');
+let overFifty = document.getElementById('overFifty');
+//define array for the whole LIs in order to make ope for them
+let rankButton = document.querySelectorAll('.rank_button');
+//define array for the whole sections in order to make loope for them
+let jsHidden = document.querySelectorAll('.js_hidden');
+// Make The first function when Li Click
+accIcon.addEventListener("click", function() {
+  jsHidden.forEach((target) => {
+    target.style.display="none";
+  })
+  manageAccount.style.display="block";
+    backRender();
+  this.style.backgroundImage = "linear-gradient(to left,#649632,#649632)";
+});
+
+// Make The second function when Li Click
+aboutIcon.addEventListener("click", function() {
+  jsHidden.forEach((target) => {
+    target.style.display="none";
+  })
+  notification.style.display="block";
+   backRender();
+  this.style.backgroundImage = "linear-gradient(to left,#649632,#649632)";
+});
+
+// Make The third function when Li Click
+workIcon.addEventListener("click", function() {
+  jsHidden.forEach((target) => {
+    target.style.display="none";
+  })
+  favList.style.display="block";
+   backRender();
+  this.style.backgroundImage = "linear-gradient(to left,#649632,#649632)";
+});
+
+// Make The fourth function when Li Click
+mailIcon.addEventListener("click", function() {
+  jsHidden.forEach((target) => {
+    target.style.display="none";
+  })
+  ordersTracking.style.display="block";
+   backRender();
+  this.style.backgroundImage = "linear-gradient(to left,#649632,#649632)";
+});
+
+// Make The fivs function when Li Click
+overIcon.addEventListener("click", function() {
+  jsHidden.forEach((target) => {
+    target.style.display="none";
+  })
+  overFifty.style.display="block";
+    backRender();
+    this.style.backgroundImage = "linear-gradient(to left,#649632,#649632)";
+});
+
+// make a function for LI color rendring 
+function backRender () {
+  rankButton.forEach((rank)=>{
+    rank.style.backgroundImage = "linear-gradient(to left,#210e7f,#080808)";
+  });
+}
+
+//***************************************************************/
+
+/****************************************************************************************************************/
+
+/*** Table Functions ********************************************************************************************/
+
+$(document).ready(function() {
+  // DataTable initialisation
+  $('#example').DataTable({
+    "paging": true,
+    "autoWidth": true,
+      });
+});
+
+$(document).ready(function() {
+  // DataTable initialisation
+  $('#exampleTwo').DataTable({
+    "paging": true,
+    "autoWidth": true,
+      });
+});
+
+$(document).ready(function() {
+  // DataTable initialisation
+  $('#exampleThree').DataTable({
+    "paging": true,
+    "autoWidth": true,
+      });
+});
+
+$(document).ready(function() {
+  // DataTable initialisation
+  $('#exampleFour').DataTable({
+    "paging": true,
+    "autoWidth": true,
+      });
+});
+
+$(document).ready(function() {
+  // DataTable initialisation
+  $('#exampleFive').DataTable({
+    "paging": true,
+    "autoWidth": true,
+      });
+});
+/*******************************************************************************************************/
+
+/*** Function to get the current date */
+
+var nowDate = new Date(); 
+// var date = nowDate.getFullYear()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getDate(); 
+var dateOne = nowDate.getDate()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getFullYear()+' '+'At'+' '+nowDate.getHours()+':'+nowDate.getMinutes(); 
+var dateTwo = nowDate.getDate()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getFullYear()+' '+'At'+' '+nowDate.getHours()+':'+nowDate.getMinutes(); 
+var dateThree = nowDate.getDate()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getFullYear()+' '+'At'+' '+nowDate.getHours()+':'+nowDate.getMinutes(); 
+var dateFour = nowDate.getDate()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getFullYear()+' '+'At'+' '+nowDate.getHours()+':'+nowDate.getMinutes(); 
+var dateFive = nowDate.getDate()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getFullYear()+' '+'At'+' '+nowDate.getHours()+':'+nowDate.getMinutes(); 
+document.getElementById("current_dateOne").innerHTML = dateOne;
+document.getElementById("current_dateTwo").innerHTML = dateTwo;
+document.getElementById("current_dateThree").innerHTML = dateThree;
+document.getElementById("current_dateFour").innerHTML = dateFour;
+document.getElementById("current_dateFive").innerHTML = dateFive;
